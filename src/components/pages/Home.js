31b82bcf76ae47d78b0f1f1424125400cadf7526
Kayla Kuhlman <kakoreo4@gmail.com>
2020-02-23 00:21:10 -0600

    added 2 new projects and fixed card stying on moblie issue

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,52 +4,9 @@ import '../../style.css';
 
 function Home() {
 
-  // const projects = {
-  //   title: "Pika-Choose! - Group Project #2",
-  //   image: "../../images/pokemon.JPG",
-  //   description: "Works as a team buidler for Pokemon teams using a SQL database.",
-  //   skills: "mySQL, handlebars, Heroku Deployment, GitHub",
-  //   contributions: "Frontend development (HTML, JS, CSS), Handlebars, Heroku Deployment, Design",
-  //   projectURL: "https://protected-spire-30180.herokuapp.com/",
-  //   repoURL: "https://github.com/ZanderMate/teamikea"
-  // }
-
-
-
   return (
-
-
-
-
     <div id="portfolio-text">
       <h2>Portfolio</h2>
-
-{/* 
-      <div className="card">
-        {projects.map(item => (
-          <div>
-          <h5>{item.title}</h5>
-          <img src={item.image} className="portfolio-image" alt="Pika-Choose Application"></img>
-          <p>Description: {item.description}</p>
-          <p><i>Skills Strengthened: {item.skills}</i></p>
-          <p>Contributions: {item.contributions}</p>
-          <a href="{item.projectURL}" target="_blank" className="btn btn-primary portfolio-btn">View Project</a>
-          <a href="{item.repoURL}" target="_blank" className="btn btn-primary portfolio-btn">View Repository on GitHub</a>
-          </div>
-    ))}
-      </div> */}
-
-
-
-
-
-
-
-
-
-
-
-
 
       <div className="card">
         <h5>Pika-Choose! - Group Project #2</h5>
@@ -69,6 +26,26 @@ function Home() {
         <a href="https://kaykuhl.github.io/project-1/html" target="_blank" className="btn btn-primary portfolio-btn">View Project</a>
         <a href="https://github.com/kaykuhl/project-1" target="_blank" className="btn btn-primary portfolio-btn">View Repository on GitHub</a>
       </div>
+
+      <div className="card">
+        <h5>React Employee Search Application</h5>
+        <img src={"/portfolio/images/index.jpg"} className="portfolio-image" alt="React Employee Search Application"></img>
+        <p>Description: Allows users to both sort and filter a list of employees using React.js components.</p>
+        <p><i>Skills Strengthened: React.js</i></p>
+        <a href="https://kaykuhl.github.io/kaykuhl.github.io/tree/master/16-react-employee-search" target="_blank" className="btn btn-primary portfolio-btn">View Project</a>
+        <a href="https://employee-react-app.firebaseapp.com/" target="_blank" className="btn btn-primary portfolio-btn">View Repository on GitHub</a>
+      </div>
+
+
+      <div className="card">
+        <h5>Eat-Da-Burger Application (Crud Example)</h5>
+        <img src={"/portfolio/images/eat-burgers.png"} className="portfolio-image" alt="Burger Application"></img>
+        <p>Description: Example of an Application using CRUD with a mySQL database. User can add, update, and delete burgers.</p>
+        <p><i>Skills Strengthened: mySQL, Heroku</i></p>
+        <a href="https://enigmatic-eyrie-27131.herokuapp.com/" target="_blank" className="btn btn-primary portfolio-btn">View Project</a>
+        <a href="https://github.com/kaykuhl/kaykuhl.github.io/tree/master/11-eat-da-burger-crud-example" target="_blank" className="btn btn-primary portfolio-btn">View Repository on GitHub</a>
+      </div>
+
       <div className="card">
         <h5>Weather Dashboard Application</h5>
         <img src={"/portfolio/images/weather-dashbaord.png"} className="portfolio-image" alt="Weather Dashboard Application"></img>
@@ -77,6 +54,7 @@ function Home() {
         <a href="https://kaykuhl.github.io/06-weather-dashboard" target="_blank" className="btn btn-primary portfolio-btn">View Project</a>
         <a href="https://github.com/kaykuhl/kaykuhl.github.io/tree/master/06-weather-dashboard" target="_blank" className="btn btn-primary portfolio-btn">View Repository on GitHub</a>
       </div>
+
       <div className="card">
         <h5>Quiz Game Application</h5>
         <img src={"/portfolio/images/quiz-game.png"} className="portfolio-image" alt="Quiz Game Application"></img>
@@ -85,6 +63,10 @@ function Home() {
         <a href="https://kaykuhl.github.io/04-quiz-game" target="_blank" className="btn btn-primary portfolio-btn">View Project</a>
         <a href="https://github.com/kaykuhl/kaykuhl.github.io/tree/master/04-quiz-game" target="_blank" className="btn btn-primary portfolio-btn">View Repository on GitHub</a>
       </div>
+<br></br>
+      <a href="https://github.com/kaykuhl/kaykuhl.github.io" target="_blank" className="btn btn-primary portfolio-btn">View my U of M BootCamp Repo</a>
+
+
     </div>
   );
 }
