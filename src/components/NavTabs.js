b7b61657177ab360ff29bb3a267cb04f861fc38e
Kayla Kuhlman <kakoreo4@gmail.com>
2020-07-12 21:35:36 -0500

    added twitter and ig icons and links

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -8,12 +8,17 @@ function NavTabs() {
       <h1>Kayla Ann Kuhlman</h1>
       <h3>Full-Stack Web Developer</h3>
       <a className="btn" href="mailto:kaylaannkuhlman@gmail.com" role="button">
-        <img alt="email icon" src={"/portfolio/images/email.png"} style={{ width: 20 + "px" }} /> Email</a>
+        <img alt="email icon" src={"/images/email.png"} style={{ width: 20 + "px" }} /> Email</a>
       <a className="btn" href="https://www.linkedin.com/in/kaykuhl/" target="_blank" role="button">
-        <img alt="linkedin icon" src={"/portfolio/images/linked in.png"} style={{ width: 20 + "px" }} /> LinkedIn</a>
+        <img alt="linkedin icon" src={"/images/linked in.png"} style={{ width: 20 + "px" }} /> LinkedIn</a>
       <a className="btn" href="https://www.github.com/kaykuhl" target="_blank" role="button">
-        <img alt="github icon" src={"/portfolio/images/githubicon.png"} style={{ width: 20 + "px" }} /> GitHub</a>
+        <img alt="github icon" src={"/images/githubicon.png"} style={{ width: 20 + "px" }} /> GitHub</a>
+      <a className="btn" href="https://www.instagram.com/blissfullycoding/" target="_blank" role="button">
+        <img alt="github icon" src={"/images/ig.png"} style={{ width: 20 + "px" }} /> Instagram</a>
+      <a className="btn" href="https://twitter.com/KaylaIsCoding" target="_blank" role="button">
+        <img alt="github icon" src={"/images/twitter.png"} style={{ width: 20 + "px" }} /> Twitter</a>
     </div>
+
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -176,15 +176,6 @@ function Home() {
 </div>
 
 
-
-
-
-
-
-
-
-
-
 <div className="container">
 <div id="about-me-text">
     <h2>About Me</h2>
@@ -215,28 +206,6 @@ function Home() {
 </div>
 
 </div>
-
-
-<div className="index-background">
-<div className="contact-links">
-<h2>Contact</h2>
-    <a className="btn" href="mailto:kaylaannkuhlman@gmail.com" role="button">
-        <img alt="email icon" src={"/images/email.png"} style={{ width: 20 + "px" }} /> KaylaAnnKuhlman@gmail.com</a>
-    <br />
-    <a className="btn" href="tel:612-720-4675" role="button">
-        <img alt="phone icon" src={"/images/phone.png"} style={{ width: 20 + "px" }} /> 612-720-4675</a>
-    <br />
-    <a className="btn" href="/portfolio/resume.pdf" target="_blank" role="button">
-        <img alt="pdf icon" src={"/images/pdf.png"} style={{ width: 20 + "px" }} /> Resume</a>
-    <br />
-    <a className="btn" href="https://www.linkedin.com/in/kaykuhl/" target="_blank" role="button">
-        <img alt="linkedin icon" src={"/images/linked in.png"} style={{ width: 20 + "px" }} /> linkedin.com/in/kaykuhl</a>
-    <br />
-    <a className="btn" href="https://www.github.com/kaykuhl" target="_blank" role="button">
-        <img alt="github icon" src={"/images/githubicon.png"} style={{ width: 20 + "px" }} /> github.com/kaykuhl</a>
-</div>
-</div>
-
 </div>
 
 
