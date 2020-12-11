7eb7639de073881fa980e074c0d4c6d2f370ca59
Kayla Kuhlman <kakoreo4@gmail.com>
2020-12-10 21:36:16 -0600

    Updates

---
+++ precache-manifest.fbb813cb32f323f8037f1017c92c421c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6300bc006a1751a2c2b19bad6f2775d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de06bdfb62843adc88a8",
+    "url": "/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "6a084bbf949dc0f84a4e",
+    "url": "/static/css/main.a5ef0ee6.chunk.css"
+  },
+  {
+    "revision": "de06bdfb62843adc88a8",
+    "url": "/static/js/2.b7c17e1d.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.b7c17e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a084bbf949dc0f84a4e",
+    "url": "/static/js/main.ca712045.chunk.js"
+  },
+  {
+    "revision": "62a47a9efc01409b9737",
+    "url": "/static/js/runtime-main.9b7e1e4b.js"
+  }
+]);
